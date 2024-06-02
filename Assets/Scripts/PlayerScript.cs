be8ee9e31e9fcc44e8aab6bc697b7cdae9746e8a
Lê Thành Lộc <106715293+thanhLoc1087@users.noreply.github.com>
2024-06-02 16:49:08 +0700

    15 - player animation

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,6 +6,9 @@ public class PlayerScript: MonoBehaviour
 {
     [Header("Player Movement")]
     public float playerSpeed = 1.9f;
+    public float playerSprint= 3.0f;
+    public float currentSpeed = 0f;
+    public float currentSprint = 0f;
 
     [Header("Player Camera")]
     public Transform playerCamera;
@@ -13,6 +16,7 @@ public class PlayerScript: MonoBehaviour
     [Header("Player Animator and Gravity")]
     public CharacterController cc;
     public float gravity = -9.81f;
+    public Animator animator;
 
     [Header("Player Jumping & Velocity")]
     public float jumpRange = 1f;
@@ -38,6 +42,8 @@ public class PlayerScript: MonoBehaviour
         playerMove();
 
         Jump();
+
+        Sprint();
     }
 
     void playerMove()
@@ -49,12 +55,29 @@ public class PlayerScript: MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
+            animator.SetBool("Walk", true);
+            animator.SetBool("Running", false);
+            animator.SetBool("Idle", false);
+            animator.SetBool("AimWalk", false);
+            animator.SetBool("IdleAim", false);
+            animator.SetTrigger("Jump");
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             cc.Move(moveDirection.normalized * playerSpeed * Time.deltaTime);
+            currentSpeed = playerSpeed;
+        } 
+        else
+        {
+            animator.SetBool("Idle", true);
+            animator.SetTrigger("Jump");
+            animator.SetBool("AimWalk", false);
+            animator.SetBool("Walk", false);
+            animator.SetBool("Running", false);
+            currentSpeed = 0f;
         }
     }
 
@@ -62,7 +85,51 @@ public class PlayerScript: MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && onSurface)
         {
+            animator.SetBool("Walk", false);
+            animator.SetTrigger("Jump");
             velocity.y = Mathf.Sqrt(jumpRange * -2 * gravity);
+        } 
+        else
+        {
+            animator.ResetTrigger("Jump");
         }
     }
+
+    void Sprint()
+    {
+        if (Input.GetButton("Sprint") && 
+            Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) &&
+            onSurface)
+        {
+            float horizontal_axis = Input.GetAxisRaw("Horizontal");
+            float vertical_axis = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                animator.SetBool("Running", true);
+                animator.SetBool("Idle", false);
+                animator.SetBool("Walk", false);
+                animator.SetBool("IdleAim", false);
+
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                cc.Move(moveDirection.normalized * playerSprint * Time.deltaTime);
+                currentSprint = playerSprint;
+            } 
+            else
+            {
+                animator.SetBool("Idle", false);
+                animator.SetBool("Walk", false);
+                currentSprint = 0f;
+            }
+        }
+    }
+
+    // player hit damange
+    // player hit die
 }
